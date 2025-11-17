cb56363e687415735f0ce0856455b201734d59e6
AKSinghongit <akhilesh4028@gmail.com>
2025-11-17 15:19:47 +0530

    Update Service.js

--- Service.js
+++ Service.js
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 import express from "express";
-const ai = new GoogleGenAI({ apiKey: "AIzaSyBBw4EtoIFMN8-9xjDWzXXXXXXX0N_3wKI"});
+const ai = new GoogleGenAI({ apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"});
 const app=express()
 app.use(express.json())
 app.post("/genrate",async(req,res)=>{
@@ -12,4 +12,5 @@ app.post("/genrate",async(req,res)=>{
   res.json({response:response.text})
 })
 app.listen(3001,()=> console.log("Started....."))
+
 
